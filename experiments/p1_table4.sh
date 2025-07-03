c14d4538ad9b4f0c9c84b0d24db78b62b1a97880
kyle <kyle@wrtn.io>
2025-07-03 16:46:38 +0900

    feaT: add p1_table4

--- experiments/p1_table4.sh
+++ experiments/p1_table4.sh
@@ -15,4 +15,36 @@ EXPERIMENT_ID="p1_table4"
 TIMESTAMP=$(date +"%Y%m%d_%H%M")
 LOG_FILE="../logs/${EXPERIMENT_ID}_${TIMESTAMP}.log"
 
+DATADIR=../data
+LOGDIR=../logs
+RESULTDIR=../output/${EXPERIMENT_ID}_${TIMESTAMP}
+
+mkdir -p "${RESULTDIR}"
+mkdir -p "${DATADIR}"
+mkdir -p "${LOGDIR}"
+
+LOG_FILE="${LOGDIR}/${EXPERIMENT_ID}_${TIMESTAMP}.log"
+
+
 log_time "$LOG_FILE" echo "Not implemented yet" 
+
+combinations=(
+  "50, 1",
+  "50, 2",
+  "200, 1",
+  "200, 2",
+)
+
+for combo in "${combinations[@]}";
+do
+        read DIM GRAM <<< "$combo"
+
+
+  echo "Downloading dataset with dimensions ${DIM} and n-grams ${GRAM}"
+  log_time "$LOG_FILE" ./fasttext supervised -input "${DATADIR}/YFCC100M/train-processing" \
+    -output "${RESULTDIR}/dim=${DIM}_gram=${GRAM}.bin" -dim ${DIM} -lr 0.25 -wordNgrams ${GRAM} \
+    -minCount 100 -bucket 100000000 -epoch 5 -thread 20 > /dev/null
+log_time ${LOG_FILE} ../fasttext test "${RESULTDIR}/dim=${DIM}_gram=${GRAM}" \
+    "${DATADIR}/YFCC100M/test-processing"
+  fi
+done
