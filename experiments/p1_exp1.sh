df414fc93d41756c54a8d70ac1c2d9f5e6094feb
kyle <kyle@wrtn.io>
2025-07-04 17:09:07 +0900

    fix

--- experiments/p1_exp1.sh
+++ experiments/p1_exp1.sh
@@ -1,4 +1,6 @@
 
+# objective: p1_table5 실험 시, train 과정에서 오랜 시간이 걸림. NS로 prediction하면, softmax로 연산하게 됨
+# 그러나, HS로 연산하면, HS loss로 연산하게 되어, 속도가 빨라질 것이라는 가설
 # 로그 함수 정의
 log_time() {
         logfile="$1"
@@ -42,7 +44,7 @@ do
   echo "Downloading dataset with dimensions ${DIM} and n-grams ${GRAM}"
   log_time "$LOG_FILE" ../fasttext supervised -input "${DATADIR}/YFCC100M/train-processing" \
     -output "${RESULTDIR}/dim${DIM}_gram${GRAM}" -dim ${DIM} -lr 0.25 -wordNgrams ${GRAM} \
-    -minCount 100 -minCountLabel 100 -bucket 100000000 -epoch 5 -thread 20 -loss ns> /dev/null
+    -minCount 100 -minCountLabel 100 -bucket 100000000 -epoch 5 -thread 20 -loss hs > /dev/null
 log_time ${LOG_FILE} ../fasttext test "${RESULTDIR}/dim${DIM}_gram${GRAM}.bin" \
     "${DATADIR}/YFCC100M/test-processing" 
 # log_time ${LOG_FILE} ../fasttext predict "../output/p1_table5_20250703_0921/dim50_gram1.bin" \
