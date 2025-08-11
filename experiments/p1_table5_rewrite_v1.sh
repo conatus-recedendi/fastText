9135907fc33eee1eae4fa89b1844cac7a1c9adcd
kyle <kyle@wrtn.io>
2025-08-12 00:19:11 +0900

    fix

--- experiments/p1_table5_rewrite_v1.sh
+++ experiments/p1_table5_rewrite_v1.sh
@@ -13,13 +13,13 @@
 
 # h=50, bigram
 
-../rewrite/bin/fastText -train ../data/YFCC100M/train-processing -output ../output/p1_table5/dim50_gram2_lr0.25.bin -size 50 -lr 0.25 -ngram 2 -min-count-vocab 100 -min-count-label 100 -bucket 100000000 -iter 5 -thread 20 -hs 1
+../rewrite/bin/fastText -train ../data/YFCC100M/train-processing -output ../output/p1_table5/dim50_gram2_lr0.25.bin -size 50 -lr 0.25 -ngram 2 -min-count-vocab 100 -min-count-label 100 -bucket 10000000 -iter 5 -thread 20 -hs 1
 
 ../rewrite/bin/test -load-model ../output/p1_table5/dim50_gram2_lr0.25.bin -test-file ../data/YFCC100M/test-processing  -topk 1 -answer-threshold 0.0
 
 
 # h=200, bigram
 
-../rewrite/bin/fastText -train ../data/YFCC100M/train-processing -output ../output/p1_table5/dim200_gram2_lr0.25.bin -size 200 -lr 0.25 -ngram 2 -min-count-vocab 100 -min-count-label 100 -bucket 100000000 -iter 5 -thread 20 -hs 1
+../rewrite/bin/fastText -train ../data/YFCC100M/train-processing -output ../output/p1_table5/dim200_gram2_lr0.25.bin -size 200 -lr 0.25 -ngram 2 -min-count-vocab 100 -min-count-label 100 -bucket 10000000 -iter 5 -thread 20 -hs 1
 
 ../rewrite/bin/test -load-model ../output/p1_table5/dim200_gram2_lr0.25.bin -test-file ../data/YFCC100M/test-processing  -topk 1 -answer-threshold 0.0
