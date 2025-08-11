b825dbdda8fbf6150fb6906ebb371dae4ec83fdb
kyle <kyle@wrtn.io>
2025-08-12 08:50:18 +0900

    fix

--- rewrite/src/test.c
+++ rewrite/src/test.c
@@ -286,15 +286,6 @@ void load_model(char *load_model_file, global_setting *gs) {
   fread(gs->left_node, sizeof(long long), gs->label_size * 2 - 1, fi);
   fread(gs->right_node, sizeof(long long), gs->label_size * 2 - 1, fi);
 
-  for (int i = 0; i< gs->label_size * 2 - 1; i++) {
-    printf("%lld ", gs->left_node[i]);
-  }
-  printf("\n");
-
-    for (int i = 0; i< gs->label_size * 2 - 1; i++) {
-    printf("%lld ", gs->right_node[i]);
-  }
-  printf("\n");
   // gs->left_node = (long long *)calloc(gs->label_size * 2 - 1, sizeof(long long));
 
   fclose(fi);
