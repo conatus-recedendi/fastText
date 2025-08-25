eaecb09bc4ddd84b531236dbe69f79990dfeb8ed
kyle <kyle@wrtn.io>
2025-08-25 16:35:43 +0900

    fix

--- eval_analogy_v1.py
+++ eval_analogy_v1.py
@@ -8,11 +8,11 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-from fasttext import load_model
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+from fasttext import load_model
 import numpy as np
 from scipy import stats
 import os
