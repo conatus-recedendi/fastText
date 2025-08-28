ee4dd567d0a7afcb7b136b5f610b44f0e42e17e8
kyle <kyle@wrtn.io>
2025-08-28 09:54:50 +0900

    fix

--- p2_table4_eval.sh
+++ p2_table4_eval.sh
@@ -1,15 +1,31 @@
-#!/bin/bash
+# #!/bin/bash
+
+# for minn in {2..6}; do
+#   for maxn in $(seq $minn 6); do
+#     model_file="./result/re-ft-sisg-wiki-de-${minn}-${maxn}.bin"
+#     echo "[INFO] Running eval with minn=${minn}, maxn=${maxn}, model=${model_file}"
+
+#     python eval.py \
+#       -m "${model_file}" \
+#       -d ./data/tasks/Gur350_DE.csv \
+#       --sisg \
+#       --minn "${minn}" \
+#       --maxn "${maxn}"
+#   done
+# done
+
 
 for minn in {2..6}; do
   for maxn in $(seq $minn 6); do
     model_file="./result/re-ft-sisg-wiki-de-${minn}-${maxn}.bin"
     echo "[INFO] Running eval with minn=${minn}, maxn=${maxn}, model=${model_file}"
 
-    python eval.py \
+    python eval_analogy_v1.py \
       -m "${model_file}" \
-      -d ./data/tasks/Gur350_DE.csv \
+      -d ./data/tasks/analogy/analogy_de.txt \
       --sisg \
       --minn "${minn}" \
-      --maxn "${maxn}"
+      --maxn "${maxn}" \
+      --topk 50000
   done
 done
